a062152eaf4f0e553e7b032d4239f4e0b34b16d3
Zeno Rocha <zno.rocha@gmail.com>
2013-09-05 11:45:36 -0300

    Add build task using Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2013, Liferay Inc. All rights reserved.
+ * Code licensed under the BSD License:
+ * https://github.com/liferay/alloy-ui/blob/master/LICENSE.md
+ *
+ * @author Zeno Rocha <zeno.rocha@liferay.com>
+ * @author Eduardo Lundgren <eduardo.lundgren@liferay.com>
+ */
+
+// -- Config -------------------------------------------------------------------
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'lib',
+                    cssDir: ''
+                }
+            }
+        },
+
+        copy: {
+            dist: {
+                files: [
+                    {
+                        src: 'bootstrap.css',
+                        dest: 'bootstrap-<%= pkg["version"] %>.css'
+                    },
+                    {
+                        src: 'responsive.css',
+                        dest: 'bootstrap-responsive-<%= pkg["version"] %>.css'
+                    }
+                ]
+            }
+        },
+
+        clean: {
+            build: [
+                'bootstrap-2.3.2.css', 'bootstrap-2.3.2.min.css',
+                'bootstrap-responsive-2.3.2.css', 'bootstrap-responsive-2.3.2.min.css'
+            ],
+            dist: [
+                'bootstrap.css', 'responsive.css',
+            ]
+        },
+
+        cssmin: {
+            dist: {
+                files: {
+                    'bootstrap-<%= pkg["version"] %>.min.css': ['bootstrap-<%= pkg["version"] %>.css'],
+                    'bootstrap-responsive-<%= pkg["version"] %>.min.css': ['bootstrap-responsive-<%= pkg["version"] %>.css']
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('build', ['clean:build', 'compass', 'copy', 'clean:dist', 'cssmin']);
+
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+};
